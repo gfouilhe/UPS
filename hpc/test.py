8ef54bfb06e91667cc1ec36bfc6f6d18f6648af1
Guilhem Fouilhé <g.g.a.fouilhe@gmail.com>
2022-12-07 09:48:39 +0100

    update tp planexpß

--- hpc/test.py
+++ hpc/test.py
@@ -1,5 +1,16 @@
 import mpi4py.MPI as mpi
+import numpy as np
 
 rank, size = mpi.COMM_WORLD.rank, mpi.COMM_WORLD.size
+comm = mpi.COMM_WORLD
+# randNum = np.zeros(1)
 
 print("hello world from process ", rank,"of", size)
+
+if rank == 1:
+    randNum = np.random.randint(3,10,size=(1,))
+    comm.send(randNum,dest=0, tag=11)
+
+if rank == 0 :
+    randNum = comm.recv(source=1, tag=11)
+    print(f'I, process {rank}, just received "{randNum}" ')
